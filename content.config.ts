778866bf69afdbd6150626d3694b53bad1b86814
Nawazish Ali <nawazishaliejaz@gmail.com>
2025-01-20 23:27:48 +0500

    - Configure nuxt content and create a test page.

---
+++ content.config.ts
@@ -0,0 +1,23 @@
+import { defineContentConfig, defineCollection, z } from '@nuxt/content'
+
+export default defineContentConfig({
+  collections: {
+    content: defineCollection({
+      type: 'page',
+      source: '**/*.md'
+    }),
+    projects: defineCollection({
+      type: 'data',
+      source: 'data/projects.json',
+      schema: z.object({
+        name: z.string(),
+        description: z.string(),
+        invlovement: z.string(),
+        Job: z.string(),
+        imageUrl: z.string(),
+        builtWith: z.string(),
+        link: z.string(),
+      })
+    })
+  }
+})
