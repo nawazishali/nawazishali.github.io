604207c0dba7ee79a92676efaa8194dfeaefea4f
Nawazish Ali <nawazishaliejaz@gmail.com>
2025-01-22 22:30:00 +0500

    - Create blog pages

---
+++ pages/blogs/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const { data: posts } = await useAsyncData('blogs', () => queryCollection('blogs').all())
+</script>
+
+<template>
+  <div>
+    <h1>List of my Blogs</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <NuxtLink :to="post.path">{{ post.title }}</NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
